dc059f7eb82b4a52d186d66e2ef6858e3f70a40f
triagex <triagex99@gmail.com>
2018-09-19 17:40:47 +0300

    Add a comment describing str_tok.

--- tokenizer.h
+++ tokenizer.h
@@ -1,6 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-char* str_tok(char**, char);
+// ppline - a pointer to a pointer to the string to be parsed.
+// delim - the character the parser will stop at the spit the first section of
+//         the string out.
+// str_tok will change the place *ppline is pointing at i.e. increase the
+// string to point to the start of the next token or null.
+char* str_tok(char** ppline, char delim);
 
 #endif
